597911bb6f728ce90ef4678fd700d5ec6793e8c8
MeyerDevelopment <David.meyer@programmer.net>
2019-04-03 20:01:44 -0500

    Initial Push

---
+++ src/main/java/deathpool/DeathPoolApplication.java
@@ -0,0 +1,13 @@
+package deathpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeathPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeathPoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/deathpool/DeathPoolApplicationTests.java
@@ -0,0 +1,16 @@
+package deathpool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeathPoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
