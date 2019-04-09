062d721fdc8962dab8cb0a2dd037fe11ea3f962b
Christian McClellan <cmcclellen93@gmail.com>
2019-04-08 19:42:20 -0500

    Create User.java
    created basic user pojo

---
+++ src/main/java/deathpool/beans/User.java
@@ -0,0 +1,67 @@
+package deathpool.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String fname;
+	private String lname;
+	private String email;
+	private boolean admin;
+	
+	//Standard user constructor, admin defaults to false
+	public User(String fname, String lname, String email) {
+		this.fname = fname;
+		this.lname = lname;
+		this.email = email;
+		this.admin = false;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+	
+}
