a54360fda03cd28c38b69c0dc31a2d267b1c29b8
MeyerDevelopment <44116669+MeyerDevelopment@users.noreply.github.com>
2019-04-18 06:11:35 -0500

    Add files via upload

---
+++ Deathpoolupdated.sql
@@ -0,0 +1,133 @@
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
+--
+-- Host: localhost    Database: deathpool
+-- ------------------------------------------------------
+-- Server version	8.0.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `app_role`
+--
+
+DROP TABLE IF EXISTS `app_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `app_role` (
+  `ROLE_ID` bigint(20) NOT NULL,
+  `ROLE_NAME` varchar(30) NOT NULL,
+  PRIMARY KEY (`ROLE_ID`),
+  UNIQUE KEY `APP_ROLE_UK` (`ROLE_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `app_role`
+--
+
+LOCK TABLES `app_role` WRITE;
+/*!40000 ALTER TABLE `app_role` DISABLE KEYS */;
+INSERT INTO `app_role` VALUES (1,'ROLE_ADMIN'),(0,'ROLE_USER');
+/*!40000 ALTER TABLE `app_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `app_user`
+--
+
+DROP TABLE IF EXISTS `app_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `app_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(20) NOT NULL,
+  `lname` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `app_user`
+--
+
+LOCK TABLES `app_user` WRITE;
+/*!40000 ALTER TABLE `app_user` DISABLE KEYS */;
+INSERT INTO `app_user` VALUES (1,'Bob','Hooper','bob.hooper@gmail.com','Abc@123Abc',0),(2,'Bob','Hope','bob.hope@gmail.com','Abc@123Abc',1),(3,'Donald','Smith','dsmith@hotmail.com','Bas#1234',0),(4,'Bob','Schnieder','bs@hotmail.com','Bas#1234',0);
+/*!40000 ALTER TABLE `app_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `characters`
+--
+
+DROP TABLE IF EXISTS `characters`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `characters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `characters`
+--
+
+LOCK TABLES `characters` WRITE;
+/*!40000 ALTER TABLE `characters` DISABLE KEYS */;
+INSERT INTO `characters` VALUES (1,'Jon Snow','unknown'),(2,'Sansa Stark','unknown'),(3,'Arya Stark','unknown'),(4,'Bran Stark','unknown'),(5,'Cersei Lannister','unknown'),(6,'Jaime Lannister','unknown'),(7,'Tyrion Lannister','unknown'),(8,'Daenerys Targaryen','unknown'),(9,'Yara Greyjoy','unknown'),(10,'Theon Greyjoy','unknown'),(11,'Melisandre','unknown'),(12,'Jorah Mormont','unknown'),(13,'The Hound','unknown'),(14,'The Mountain','unknown'),(15,'Samwell Tarley','unknown'),(16,'Gilly','unknown'),(17,'Sam (child)','unknown'),(18,'Lord Varys','unknown'),(19,'Brienne of Tarth','unknown'),(20,'Davos Seaworth','unknown'),(21,'Bronn','unknown'),(22,'Podrick Payne','unknown'),(23,'Tormund Giantsbane','unknown'),(24,'Grey Worm','unknown'),(25,'Gendry','unknown'),(26,'Beric Dondarrion','unknown'),(27,'Euron Greyjoy','unknown');
+/*!40000 ALTER TABLE `characters` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `deathpool`
+--
+
+DROP TABLE IF EXISTS `deathpool`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `deathpool` (
+  `userid` bigint(20) NOT NULL,
+  `characterid` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`userid`,`characterid`),
+  KEY `characterid` (`characterid`),
+  CONSTRAINT `deathpool_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `app_user` (`id`),
+  CONSTRAINT `deathpool_ibfk_2` FOREIGN KEY (`characterid`) REFERENCES `characters` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `deathpool`
+--
+
+LOCK TABLES `deathpool` WRITE;
+/*!40000 ALTER TABLE `deathpool` DISABLE KEYS */;
+/*!40000 ALTER TABLE `deathpool` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-18  6:10:54
